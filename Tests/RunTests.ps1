0ef6eee93e1e13af5accfdd36e49f6efa90b71a5
Lance England <lance.england@gmail.com>
2019-04-24 21:34:43 -0400

    Tags

--- Tests/RunTests.ps1
+++ Tests/RunTests.ps1
@@ -4,4 +4,4 @@ $ModuleBase = Split-Path -Path $PSScriptRoot -Parent
 Remove-Module EdiTools -ErrorAction Ignore
 Import-Module "$ModuleBase\EdiTools.psm1"
 
-Invoke-Pester -Path "$ModuleBase\Tests\"
+Invoke-Pester -Path "$ModuleBase\Tests\" -Tag 'MatchInfo'
