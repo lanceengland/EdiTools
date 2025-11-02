a4f2d60de5c674786e7f491ae173d37f9a78ec16
Lance England <lance.england@gmail.com>
2025-11-02 11:58:38 -0500

    Fixed ShortId property

--- CSharp/EdiTools/Edi837/TransactionSet.cs
+++ CSharp/EdiTools/Edi837/TransactionSet.cs
@@ -14,16 +14,25 @@ namespace EdiTools.Edi837
         }
         public string VersionShortId
         {
+            /*
+             P: 005010X222A1 Standards Approved for Publication by ASC X12 Procedures Review Board through October 2003 
+             I: 005010X223A2 Standards Approved for Publication by ASC X12 Procedures Review Board through October 2003 
+             D: 005010X224A2 Standards Approved for Publication by ASC X12 Procedures Review Board through October 2003 
+             */
+
             get
             {
                 switch (this.VersionIdentifier)
                 {
                     case "005010X222A1":
                         return "P";
-                    case "005010X222A2":
+
+                    case "005010X223A2":
                         return "I";
-                    case "005010X2224A2":
+
+                    case "005010X224A2":
                         return "D";
+
                     default:
                         return "Unknown";
                 }
